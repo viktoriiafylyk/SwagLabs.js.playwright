501e351ee3b59f8dbe904672992d6d89bd8d71e4
viktoriiafylyk <vfilik194@gmail.com>
2025-08-11 11:52:35 +0300

    Refactored test

--- tests/valid-login.spec.js
+++ tests/valid-login.spec.js
@@ -1,17 +1,23 @@
 import {test, expect} from '@playwright/test';
 import {LoginPage} from "../src/pages/login-page";
+import {InventoryPage} from "../src/pages/inventory-page";
 
 test.describe('Valid Login Test', () => {
     let loginPage;
+    let inventoryPage;
+
 
     test.beforeEach(async ({page}) => {
         loginPage = new LoginPage(page);
+        inventoryPage = new InventoryPage(page);
         await loginPage.visit();
     });
 
-    test('test', async ({ page }) => {
+    test('Verify login with valid data', async ({ page }) => {
         await loginPage.enterUsername('standard_user');
         await loginPage.enterPassword('secret_sauce');
         await loginPage.clickLoginButton();
+        await expect(inventoryPage.inventoryContainer).toBeVisible();
+        await expect(inventoryPage.inventoryItem.first()).toBeVisible();
     });
 });
