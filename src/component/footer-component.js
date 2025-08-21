cbec8723162d19f973a2da81db076f50e712ce91
viktoriiafylyk <vfilik194@gmail.com>
2025-08-21 21:12:42 +0300

    added test that verifies footer links

--- src/component/footer-component.js
+++ src/component/footer-component.js
@@ -1,1 +1,50 @@
-import {Page, Locator} from '@playwright/test';
+const SELECTORS = {
+    footer: '.footer',
+    twitterLink: 'a[data-test="social-twitter"]',
+    facebookLink: 'a[data-test="social-facebook"]',
+    linkedInLink: 'a[data-test="social-linkedin"]',
+    copyright: '.footer .footer_copy'
+}
+
+export class FooterComponent {
+
+    constructor(page) {
+        this.page = page;
+        this.footer = page.locator(SELECTORS.footer);
+        this.twitterLink = page.locator(SELECTORS.twitterLink);
+        this.facebookLink = page.locator(SELECTORS.facebookLink);
+        this.linkedInLink = page.locator(SELECTORS.linkedInLink);
+        this.copyright = page.locator(SELECTORS.copyright);
+    }
+
+    async isFooterVisible() {
+        return this.page.isVisible(SELECTORS.footer);
+    }
+
+    async isTwitterLinkDisplayed() {
+        return this.page.isVisible(SELECTORS.twitterLink);
+    }
+
+    async isFacebookLinkDisplayed() {
+        return this.page.isVisible(SELECTORS.facebookLink);
+    }
+
+    async isLinkedInLinkDisplayed() {
+        return this.page.isVisible(SELECTORS.linkedInLink);
+    }
+
+    async clickAndSwitchToNewTab(link) {
+        await link.scrollIntoViewIfNeeded();
+        const [newPage] = await Promise.all([
+            this.page.context().waitForEvent('page'),
+            link.click({force: true})
+        ]);
+        return newPage;
+    }
+
+    async getCopyrightText() {
+        return this.page.textContent(SELECTORS.copyright);
+    }
+}
+
+export {expect} from '@playwright/test';
---
+++ tests/footer-links.spec.js
@@ -0,0 +1,28 @@
+import {test, expect} from '../fixtures/login-fixture';
+import {FooterComponent} from '../src/component/footer-component';
+
+test.describe('Footer social media links', () => {
+    test('Verify footer links and copyright text', async ({inventoryPage}) => {
+        const footer = new FooterComponent(inventoryPage.page);
+
+        expect(await footer.isFooterVisible()).toBeTruthy();
+
+        expect(await footer.isTwitterLinkDisplayed()).toBeTruthy();
+        const twitterPage = await footer.clickAndSwitchToNewTab(footer.twitterLink);
+        expect(twitterPage.url()).toContain('x.com');
+        await twitterPage.close();
+
+        expect(await footer.isFacebookLinkDisplayed()).toBeTruthy();
+        const facebookPage = await footer.clickAndSwitchToNewTab(footer.facebookLink);
+        expect(facebookPage.url()).toContain('facebook.com');
+        await facebookPage.close();
+
+        expect(await footer.isLinkedInLinkDisplayed()).toBeTruthy();
+        const linkedInPage = await footer.clickAndSwitchToNewTab(footer.linkedInLink);
+        expect(linkedInPage.url()).toContain('linkedin.com');
+        await linkedInPage.close();
+
+        const copyrightText = await footer.getCopyrightText();
+        expect(copyrightText).toContain('© 2025 Sauce Labs. All Rights Reserved. Terms of Service');
+    });
+});
